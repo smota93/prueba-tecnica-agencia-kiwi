1c79e4b9baa036c95c7c0569f34c057a848c5fa6
smota93 <sergioleomota12@hotmail.com>
2025-07-27 20:33:06 -0400

    Add Search component with input field and magnifying glass icon for enhanced search functionality

---
+++ app/ui/search.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+
+export default function Search({ placeholder }: { placeholder: string }) {
+  return (
+    <div className="relative flex flex-1 flex-shrink-0">
+      <label htmlFor="search" className="sr-only">
+        Search
+      </label>
+      <input
+        className="peer block w-full rounded-2xl bg-gray-100 py-[8px] pl-10 text-sm placeholder:text-gray-500"
+        placeholder={placeholder}
+      />
+      <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+    </div>
+  );
+}
